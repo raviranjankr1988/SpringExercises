79432e33f4d2fbc15a90f288870d3a9b7ded5bb9
Ravi Ranjan Kumar <raviranjan.kumar@tivo.com>
2017-12-20 23:43:59 +0530

    AOP Pointcut

--- SpringAOP/PointcutsWildcardExpressions/src/main/java/com/springexercises/springaop/aspect/LoggingAspect.java
+++ SpringAOP/PointcutsWildcardExpressions/src/main/java/com/springexercises/springaop/aspect/LoggingAspect.java
@@ -2,18 +2,22 @@ package com.springexercises.springaop.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
     //@Before("execution(public String com.springexercises.springaop.model.Circle.getName())")  //getName for circle class
-    @Before("execution(* get*())")  //all get method
+    @Before("allGetters()")  //all get method
     public void LoggingAdvice() {
         System.out.println("LoggingAdvice method called");
     }
 
-    @Before("execution(* get*())")
+    @Before("allGetters()")
     public void SecondAdvice() {
         System.out.println("SecondAdvice method called");
     }
+
+    @Pointcut("execution(* get*())")
+    public void allGetters(){}
 }
